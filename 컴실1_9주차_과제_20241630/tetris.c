8fbd00bda7fca79cd157f6eb88d1c0ac12150297
ychanyoung <yooncy0511@gmail.com>
2025-05-28 22:40:04 +0900

    랭킹 시스템 구현

---
+++ 컴실1_9주차_과제_20241630/tetris.c
@@ -0,0 +1,606 @@
+﻿#include "tetris.h"
+#include <unistd.h>
+
+static struct sigaction act, oact;
+
+int main(){
+	int exit=0;
+
+	createRankList();
+	initscr();
+	noecho();
+	keypad(stdscr, TRUE);	
+
+	srand((unsigned int)time(NULL));
+
+	while(!exit){
+		clear();
+		switch(menu()){
+		case MENU_PLAY: play(); break;
+		case MENU_RANK: rank(); break;
+		case MENU_EXIT: exit=1; break;
+		default: break;
+		}
+	}
+
+	endwin();
+	writeRankFile();
+	system("clear");
+	return 0;
+}
+
+void InitTetris(){
+	int i,j;
+
+	for(j=0;j<HEIGHT;j++)
+		for(i=0;i<WIDTH;i++)
+			field[j][i]=0;
+
+	nextBlock[0]=rand()%7;
+	nextBlock[1]=rand()%7;
+	nextBlock[2] = rand() % 7;
+	blockRotate=0;
+	blockY=-1;
+	blockX=WIDTH/2-2;
+	score=0;	
+	gameOver=0;
+	timed_out=0;
+
+	DrawOutline();
+	DrawField();
+	DrawBlockWithFeatures(blockY,blockX,nextBlock[0],blockRotate);
+	DrawNextBlock(nextBlock);
+	PrintScore(score);
+}
+
+void DrawOutline(){	
+	int i,j;
+	/* 블럭이 떨어지는 공간의 태두리를 그린다.*/
+	DrawBox(0,0,HEIGHT,WIDTH);
+
+	/* next block을 보여주는 공간의 태두리를 그린다.*/
+	move(2,WIDTH+10);
+	printw("NEXT BLOCK");
+	DrawBox(3,WIDTH+10,4,8);
+	move(2, WIDTH + 10);
+	DrawBox(9, WIDTH + 10, 4, 8);
+
+	/* score를 보여주는 공간의 태두리를 그린다.*/
+	move(15,WIDTH+10);
+	printw("SCORE");
+	DrawBox(16,WIDTH+10,1,8);
+}
+
+int GetCommand(){
+	int command;
+	command = wgetch(stdscr);
+	switch(command){
+	case KEY_UP:
+		break;
+	case KEY_DOWN:
+		break;
+	case KEY_LEFT:
+		break;
+	case KEY_RIGHT:
+		break;
+	case ' ':	/* space key*/
+		/*fall block*/
+		break;
+	case 'q':
+	case 'Q':
+		command = QUIT;
+		break;
+	default:
+		command = NOTHING;
+		break;
+	}
+	return command;
+}
+
+int ProcessCommand(int command){
+	int ret=1;
+	int drawFlag=0;
+	switch(command){
+	case QUIT:
+		ret = QUIT;
+		break;
+	case KEY_UP:
+		if((drawFlag = CheckToMove(field,nextBlock[0],(blockRotate+1)%4,blockY,blockX)))
+			blockRotate=(blockRotate+1)%4;
+		break;
+	case KEY_DOWN:
+		if((drawFlag = CheckToMove(field,nextBlock[0],blockRotate,blockY+1,blockX)))
+			blockY++;
+		break;
+	case KEY_RIGHT:
+		if((drawFlag = CheckToMove(field,nextBlock[0],blockRotate,blockY,blockX+1)))
+			blockX++;
+		break;
+	case KEY_LEFT:
+		if((drawFlag = CheckToMove(field,nextBlock[0],blockRotate,blockY,blockX-1)))
+			blockX--;
+		break;
+	default:
+		break;
+	}
+	if(drawFlag) DrawChange(field,command,nextBlock[0],blockRotate,blockY,blockX);
+	return ret;	
+}
+
+void DrawField(){
+	int i,j;
+	for(j=0;j<HEIGHT;j++){
+		move(j+1,1);
+		for(i=0;i<WIDTH;i++){
+			if(field[j][i]==1){
+				attron(A_REVERSE);
+				printw(" ");
+				attroff(A_REVERSE);
+			}
+			else printw(".");
+		}
+	}
+}
+
+
+void PrintScore(int score){
+	move(17,WIDTH+11);
+	printw("%8d",score);
+}
+
+void DrawNextBlock(int *nextBlock){
+	int i, j;
+	for( i = 0; i < 4; i++ ){
+		move(4+i,WIDTH+13);
+		for( j = 0; j < 4; j++ ){
+			if( block[nextBlock[1]][0][i][j] == 1 ){
+				attron(A_REVERSE);
+				printw(" ");
+				attroff(A_REVERSE);
+			}
+			else printw(" ");
+		}
+	}
+	for (i = 0; i < 4; i++) {
+		move(10 + i, WIDTH + 13);
+		for (j = 0; j < 4; j++) {
+			if (block[nextBlock[2]][0][i][j] == 1) {
+				attron(A_REVERSE);
+				printw(" ");
+				attroff(A_REVERSE);
+			}
+			else printw(" ");
+		}
+	}
+}
+
+void DrawBlockWithFeatures(int y, int x, int blockID, int blockRotate) {
+	DrawShadow(y, x, blockID, blockRotate);
+	DrawBlock(blockY, blockX, nextBlock[0], blockRotate, ' ');
+
+}
+
+
+void DrawBlock(int y, int x, int blockID,int blockRotate,char tile){
+	int i,j;
+	
+
+	for(i=0;i<4;i++)
+		for(j=0;j<4;j++){
+			if(block[blockID][blockRotate][i][j]==1 && i+y>=0){
+				move(i+y+1,j+x+1);
+				attron(A_REVERSE);
+				printw("%c",tile);
+				attroff(A_REVERSE);
+			}
+		}
+
+	move(HEIGHT,WIDTH+10);
+}
+
+void DrawBox(int y,int x, int height, int width){
+	int i,j;
+	move(y,x);
+	addch(ACS_ULCORNER);
+	for(i=0;i<width;i++)
+		addch(ACS_HLINE);
+	addch(ACS_URCORNER);
+	for(j=0;j<height;j++){
+		move(y+j+1,x);
+		addch(ACS_VLINE);
+		move(y+j+1,x+width+1);
+		addch(ACS_VLINE);
+	}
+	move(y+j+1,x);
+	addch(ACS_LLCORNER);
+	for(i=0;i<width;i++)
+		addch(ACS_HLINE);
+	addch(ACS_LRCORNER);
+}
+
+void play(){
+	int command;
+	clear();
+	act.sa_handler = BlockDown;
+	sigaction(SIGALRM,&act,&oact);
+	InitTetris();
+	do{
+		if(timed_out==0){
+			alarm(1);
+			timed_out=1;
+		}
+
+		command = GetCommand();
+		if(ProcessCommand(command)==QUIT){
+			alarm(0);
+			DrawBox(HEIGHT/2-1,WIDTH/2-5,1,10);
+			move(HEIGHT/2,WIDTH/2-4);
+			printw("Good-bye!!");
+			refresh();
+			getch();
+
+			return;
+		}
+	}while(!gameOver);
+
+	alarm(0);
+	getch();
+	DrawBox(HEIGHT/2-1,WIDTH/2-5,1,10);
+	move(HEIGHT/2,WIDTH/2-4);
+	printw("GameOver!!");
+	refresh();
+	newRank(score);
+	getch();
+}
+
+char menu(){
+	printw("1. play\n");
+	printw("2. rank\n");
+	printw("3. recommended play\n");
+	printw("4. exit\n");
+	return wgetch(stdscr);
+}
+
+/////////////////////////첫주차 실습에서 구현해야 할 함수/////////////////////////
+
+int CheckToMove(char f[HEIGHT][WIDTH],int currentBlock,int blockRotate, int blockY, int blockX){
+	// user code
+	int flag = 1;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (block[currentBlock][blockRotate][i][j] == 1) {
+				if ((blockX + j) < 0 || (blockX + j) >= WIDTH) {
+					return 0;
+				}
+				if ((blockY + i) < 0 || (blockY + i) >= HEIGHT) {
+					return 0;
+				}
+				if (f[blockY + i][blockX + j] == 1) return 0;
+			}
+		}
+	}
+
+	return 1;
+}
+
+void DrawChange(char f[HEIGHT][WIDTH],int command,int currentBlock,int blockRotate, int blockY, int blockX){
+	// user code
+
+	//1. 이전 블록 정보를 찾는다. ProcessCommand의 switch문을 참조할 것
+	//2. 이전 블록 정보를 지운다. DrawBlock함수 참조할 것.
+	//3. 새로운 블록 정보를 그린다. 
+
+	int r = blockRotate;
+	int x = blockX;
+	int y = blockY;
+
+	switch (command) {
+	case KEY_UP:
+		r = (blockRotate + 3) % 4;
+		break;
+	case KEY_DOWN:
+		y = blockY-1;
+		break;
+	case KEY_RIGHT:
+		x = blockX-1;
+		break;
+	case KEY_LEFT:
+		x = blockX+1;
+		break;
+	default:
+		break;
+	}
+	int shadow = y;
+	while (CheckToMove(field, nextBlock[0], r, shadow + 1, x)) {
+		shadow++;
+	}
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (block[currentBlock][r][i][j] == 1 && i + shadow >= 0) {
+				move(i + shadow + 1, j + x + 1);
+				printw("%c", '.');
+			}
+		}
+	}
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (block[currentBlock][r][i][j] == 1 && i + y >= 0) {
+				move(i + y + 1, j + x + 1);
+				printw("%c", '.');
+			}
+		}
+	}
+
+	DrawBlockWithFeatures(blockY, blockX, nextBlock[0], blockRotate);
+	move(HEIGHT, WIDTH + 10);
+}
+
+void BlockDown(int sig){
+	// user code
+
+	//강의자료 p26-27의 플로우차트를 참고한다.
+	if (CheckToMove(field, nextBlock[0], blockRotate, blockY + 1, blockX)) {
+		blockY++;
+		DrawChange(field, KEY_DOWN, nextBlock[0], blockRotate, blockY, blockX);
+	}
+	else {
+		if(blockY == -1) gameOver = 1;
+		score += 10 * AddBlockToField(field, nextBlock[0], blockRotate, blockY, blockX);
+		score += DeleteLine(field);
+		nextBlock[0] = nextBlock[1];
+		nextBlock[1] = nextBlock[2];
+		nextBlock[2] = rand() % 7;
+		blockRotate = 0;
+		blockY = -1;
+		blockX = WIDTH / 2 - 2;
+		DrawNextBlock(nextBlock);
+		PrintScore(score);
+		DrawField();
+
+	}
+	timed_out = 0;
+}
+
+int AddBlockToField(char f[HEIGHT][WIDTH],int currentBlock,int blockRotate, int blockY, int blockX){
+	// user code
+	int touched = 0;
+
+	//Block이 추가된 영역의 필드값을 바꾼다.
+	for (int i = 0; i<4; i++) {
+		for (int j = 0; j<4; j++) {
+			if (block[currentBlock][blockRotate][i][j]==1) {
+				if (blockY + i + 1 >= HEIGHT) touched++;
+				else if (f[blockY + i + 1][blockX + j] == 1) touched++;
+				f[blockY+i][blockX+j] = 1;
+			}
+		}
+	}
+	return touched;
+}
+
+int DeleteLine(char f[HEIGHT][WIDTH]){
+	// user code
+
+	//1. 필드를 탐색하여, 꽉 찬 구간이 있는지 탐색한다.
+	//2. 꽉 찬 구간이 있으면 해당 구간을 지운다. 즉, 해당 구간으로 필드값을 한칸씩 내린다.
+	int line = HEIGHT-1;
+	int flag;
+	int ret = 0;
+	while (line>=0) {
+		flag = 1;
+		for (int i = 0; i < WIDTH; i++) {
+			if (f[line][i] == 0) {
+				flag = 0;
+				break;
+			}
+		}
+		if (flag) {
+			for (int i = line; i > 0; i--) {
+				for (int j = 0; j < WIDTH; j++) {
+					f[i][j] = f[i - 1][j];
+				}
+			}
+			for (int j = 0; j < WIDTH; j++) f[0][j] = 0;
+			ret++;
+		}
+		else line--;
+
+
+	}
+	return ret*ret * 100;
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+void DrawShadow(int y, int x, int blockID,int blockRotate){
+	// user code
+	int shadow = y;
+	while (CheckToMove(field, nextBlock[0], blockRotate, shadow + 1, x)) {
+		shadow++;
+	}
+
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 4; j++) {
+			if (block[blockID][blockRotate][i][j] == 1 && i + shadow >= 0) {
+				move(i + shadow + 1, j + x + 1);
+				attron(A_REVERSE);
+				printw("%c", '/');
+				attroff(A_REVERSE);
+			}
+		}
+
+	move(HEIGHT, WIDTH + 10);
+}
+
+void createRankList() {
+	// user code
+	FILE* file = fopen("rank.txt", "r");
+	if (file == NULL) return;
+	fscanf(file, "%d", &ranknum);
+	for (int i = 0; i < ranknum; i++) {
+		char nm[NAMELEN];
+		int sc;
+		fscanf(file, "%s %d", nm, &sc);
+		int idx = binary(i, nm, sc);
+
+		for (int j = i; j > idx; j--) {
+			rankarr[j] = rankarr[j - 1];
+		}
+
+		rankarr[idx].score = sc; strcpy(rankarr[idx].name, nm);
+	}
+}
+
+int binary(int n, char name[], int sc) {
+	int left = 0;
+	int right = n;
+	while (left < right) {
+		int mid = (left + right) / 2;
+		if (rankarr[mid].score == sc) {
+			return mid + 1;
+		}
+		else if (rankarr[mid].score < sc) {
+			right = mid;
+		}
+		else left = mid + 1;
+	}
+	return left;
+}
+
+void rank() {
+
+	int X = 1, Y = ranknum;
+	char buf[100];
+	int flag = 0;
+	int idx;
+	buf[0] = '\n';
+	// user code
+	initscr();
+	noecho();
+	keypad(stdscr, TRUE);
+
+	while (1) 
+	{
+		clear();
+		noecho();
+		printw("1. list ranks from X to Y\n");
+		printw("2. list ranks by a specific name\n");
+		printw("3. delete\n");
+		printw("4. exit\n");
+		switch (wgetch(stdscr)) {
+		case '1':
+			X = 1; Y = ranknum;
+			echo();
+
+			printw("X :"); 
+			scanw("%d", &X);
+			printw("Y :"); 
+			scanw("%d", &Y);
+
+			if (Y > ranknum || X <= 0 || Y <= 0 || X>Y) {
+				initscr();
+				noecho();
+				keypad(stdscr, TRUE);	
+				break;
+			}
+
+			for (int i = X - 1; i < Y; i++) {
+				printw("%s %d\n", rankarr[i].name, rankarr[i].score);
+			}
+			scanw("%s", buf);
+			break;
+		case '2':
+			flag = 0;
+			echo();
+			printw("input name : ");
+			scanw("%s", buf);
+			for (int i = 0; i < ranknum; i++) {
+				if (strcmp(rankarr[i].name, buf) == 0) {
+					printw("%s %d\n", buf, rankarr[i].score);
+					flag = 1;
+				}
+			}
+			if (flag == 0) printw("search failure: no name in the list\n");
+
+			scanw("%s", buf);
+			noecho();
+			break;
+		case '3':
+			flag = 0;
+			echo();
+			printw("input delele rank : ");
+			scanw("%d", &idx);
+			if (ranknum < idx) {
+				printw("search failure: the rank not in the list\n");
+			}
+			else {
+				printw("result: the rank deleted\n");
+				for (int i = idx-1; i < ranknum-1; i++) {
+					rankarr[i] = rankarr[i + 1];
+				}
+				ranknum--;
+			}
+
+			scanw("%s", buf);
+			break;
+		case '4':
+			clear();
+			initscr();
+			noecho();
+			keypad(stdscr, TRUE);
+			return;
+
+		}
+	}
+}
+
+void writeRankFile() {
+	// user code
+	FILE* file = fopen("rank.txt", "w");
+
+	fprintf(file, "%d\n", ranknum);
+	for (int i = 0; i < ranknum; i++) {
+		fprintf(file, "%s %d\n", rankarr[i].name, rankarr[i].score);
+	}
+	fclose(file);
+}
+
+void newRank(int score) {
+	// user code
+
+	clear();
+	refresh();
+	endwin();
+
+
+	char nm[NAMELEN];
+	scanf("%s", nm);
+	int idx = binary(ranknum, nm, score);
+
+	for (int j = ranknum; j > idx; j--) {
+		rankarr[j] = rankarr[j - 1];
+	}
+	rankarr[idx].score = score; strcpy(rankarr[idx].name, nm);
+
+	ranknum++;
+
+	initscr();
+	noecho();
+	keypad(stdscr, TRUE);
+}
+
+void DrawRecommend(int y, int x, int blockID, int blockRotate) {
+	// user code
+}
+
+int recommend(RecNode* root) {
+	int max = 0; // 미리 보이는 블럭의 추천 배치까지 고려했을 때 얻을 수 있는 최대 점수
+
+	// user code
+
+	return max;
+}
+
+void recommendedPlay() {
+	// user code
+}
